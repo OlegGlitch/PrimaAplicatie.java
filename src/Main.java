15d2aa41962a9d0c324533a4c4be2f3d04453d1e
macar <ipetea28@gmail.com>
2025-02-13 10:06:50 +0200

    al doilea push prin protocolul ssh

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@
 public class Main {
     // JAR - Java Archive
     public static void main(String[] args) {
-        System.out.println("Invat limbajul java!");
+        System.out.println("Invat limbajul bine java!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
