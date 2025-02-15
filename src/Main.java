3de2c56d5dc2c87d23b7c7f5686eef3e47ac751b
OlegGlitch <macaricioleg8@gmail.com>
2025-02-15 23:58:31 +0200

    Modificare clona 1.2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     // JAR - Java Archive
     public static void main(String[] args) {
         System.out.println("Invat limbajul java bine de tot!");
+        System.out.println("Invat limbajul java bine upload");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
