a47e6e8406e634b581bf410049696a2a4c1e7c30
OlegGlitch <macaricioleg8@gmail.com>
2025-02-16 15:08:42 +0200

    Adaugare comentarii pe o singura linie

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     // JAR - Java Archive
     public static void main(String[] args) {
-        System.out.println("Invat limbajul java bine de tot!");
+        System.out.println("Invat limbajul javabine de tot!");
         System.out.println("Invat limbajul java bine upload");
-
+        // aici vrem sa afisam pe consala Intellij un mesaj ales de noi
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
