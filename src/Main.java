7061ea2dc582585749ba29370498755b56cce490
OlegGlitch <macaricioleg8@gmail.com>
2025-02-19 16:16:16 +0200

    Am adaugat JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 
 public class Main {
-    // JAR - Java Archive
-    public static void main(/* args este parametru metodei main */String[] args) {// in practica nu prea se foloseste aceasta
+
+
+    /**
+     * Metoda main este metoda apelata de masina virtuala Java si care porneste o aplicatie Java
+     * Putem sa transmitem argumente adutionale prin parametrul <b>args</b>
+     * @param args Acesta este parametru cu care transmitem argumente programului nostru
+     * @author Macarici Oleg
+     */
+    public static void main(String[] args) {
         System.out.println("Invat limbajul javabine de tot!");
-        System.out.println("Invat limbajul java bine upload");
 
-         /*comentarii mjulti linie
-         aici vrem sa afisam pe consala Intellij un mesaj ales de noi
-         textul de aici tot face parte din comentariul multi-linie
-         */
-
-        /*
-        aici este un alt exemplu de
-        comentariu
-        multi-linie
-         */
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
